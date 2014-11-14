aba62410bebe8fe24db61aa7558ed179e658c9ac
naaambo <sam.uk.jones@gmail.com>
2014-11-14 17:14:33 +0000

    Base Added
    Added Base functionality: Creates Server and Binds it to a host name and
    port. Adds Server name
    
    Does not have any user interaction.

---
+++ IIS Control/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Web.Administration;
+
+namespace IIS_Control
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServerManager serverManager = new ServerManager();
+			
+
+			//Settings the Name, Protocol, Bindings (in this case, Port80 and a HostName, does not include Binding Information) and Default Path
+			Site site = serverManager.Sites.Add("TestSite", "http", "*:80:www.onboardtest.local", "D:\\TestSite");
+			site.Name = "OnBoardTest";
+
+			//Setting ApplicationPoolName
+			site.ApplicationDefaults.ApplicationPoolName = "OnBoardDev";
+
+			//Starting Server and applying bindings.
+			site.ServerAutoStart = true;
+			serverManager.CommitChanges();
+
+			Console.ReadLine();
+			site.Stop();
+			Console.ReadLine();
+
+		}
+	}
+}
