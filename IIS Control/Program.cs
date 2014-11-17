5134749ded4e3b77508640fac556187049041e86
naaambo <sam.uk.jones@gmail.com>
2014-11-17 11:06:45 +0000

    Adding Applications and Settings Files
    Added 2 new application to the site. Adding a new virtual directory for
    "settings" to point to the respective settings.xml files for each new
    application.

--- IIS Control/Program.cs
+++ IIS Control/Program.cs
@@ -11,22 +11,38 @@ namespace IIS_Control
 	{
 		static void Main(string[] args)
 		{
+			//Need to split everything up into Chunks.
+			//Define Version/Application Source/Settings Folder
+			//Automatically add the settings file.
+			//Edit Hosts File
+
 			ServerManager serverManager = new ServerManager();
-			
+			Application connect;
+			Application boardpadweb;
 
 			//Settings the Name, Protocol, Bindings (in this case, Port80 and a HostName, does not include Binding Information) and Default Path
-			Site site = serverManager.Sites.Add("TestSite", "http", "*:80:www.onboardtest.local", "D:\\TestSite");
+			Site site = serverManager.Sites.Add("TestSite", "http", "*:80:www.onboardtest.local", @"D:\Default");
 			site.Name = "OnBoardTest";
+
+			connect = site.Applications.Add("/connect", @"D:\Source\Connect\v1.9\OnBoardNextGeneration\OnBoard");
+			connect.VirtualDirectories.Add("/settings", @"D:\Default\1.9a\connect");
+
+			boardpadweb = site.Applications.Add("/boardpadweb", @"D:\Source\Connect\v1.9\OnBoardNextGeneration\OnBoard");
+			boardpadweb.VirtualDirectories.Add("/settings", @"D:\Default\1.9a\boardpadweb");
+
 
 			//Setting ApplicationPoolName
 			site.ApplicationDefaults.ApplicationPoolName = "OnBoardDev";
-
+			
 			//Starting Server and applying bindings.
 			site.ServerAutoStart = true;
+
+//			site.Applications.Add("/boardpadweb", @"D\Source\Connect\v1.9\OnBoardNextGeneration\OnBoard").VirtualDirectories.Add(
+//																										"/settings",
+//																										@"D:\Default\1.9a\boardpadweb");
+
 			serverManager.CommitChanges();
 
-			Console.ReadLine();
-			site.Stop();
 			Console.ReadLine();
 
 		}
